8d3c839db16c4a727af0bbff7dbe7ef9003f3af9
ghost-of-the-mind <hardgovernance@gmail.com>
2022-01-21 04:22:56 +0200

    Release version 1.0.2

--- app-front/src/components/logic/fetch-config.js
+++ app-front/src/components/logic/fetch-config.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 
 const nodeEnv = process.env.NODE_ENV !== 'production';
-
 const digitalOceanPort = process.env.PORT;
 
 const devProxy = process.env.REACT_APP_dev_proxy;
 const prodProxy = process.env.REACT_APP_prod_proxy;
 
-const digitalOceanProxy = prodProxy + digitalOceanPort;
+const digitalOcealProxy = prodProxy + '/' + digitalOceanPort;
 
-const customProxy = nodeEnv ? devProxy : digitalOceanProxy;
+const customProxy = nodeEnv ? devProxy : digitalOcealProxy;
 
 export default customProxy;
 
