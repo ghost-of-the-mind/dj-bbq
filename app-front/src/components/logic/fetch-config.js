9aafda6802a2b8ea525b7511a715d2bb20163b28
ghost-of-the-mind <hardgovernance@gmail.com>
2022-01-21 22:19:58 +0200

    Release version 1.0.10

--- app-front/src/components/logic/fetch-config.js
+++ app-front/src/components/logic/fetch-config.js
@@ -19,7 +19,7 @@ const customProxy = nodeEnv ? devProxy : digitalOcealProxy;
 // const customProxy = 'https://dj-bbq-app-server/80';
 // const customProxy = 'https://dj-bbq-app-server';
 
-const customProxy = 'https://dj-bbq-i5gdc.ondigitalocean.app/8080';
+const customProxy = 'https://dj-bbq.dj-bbq-app-server';
 
 export default customProxy;
 
